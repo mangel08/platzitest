d25e3c5277b4768cf98ad37b820bcdcdea805f7c
Miguelangel Palma <mangel.2711@gmail.com>
2020-09-30 15:47:40 -0400

    probando callbacks

---
+++ __test__/strings.test.js
@@ -0,0 +1,12 @@
+describe('Comprobar cadenas de texto', () => {
+  const text = 'Un bonito texto';
+  test('Debe contener el siguiente texto', () => {
+    expect(text).toMatch(/bonito/);
+  });
+  test('No contiene el siguiente texto', () => {
+    expect(text).not.toMatch(/es/);
+  });
+  test('Comprobar el tamaño de un texto', () => {
+    expect(text).toHaveLength(15);
+  });
+});
