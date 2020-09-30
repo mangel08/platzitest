11f467eb1d169737cdcd9ff6255c4c359c97a2de
Miguelangel Palma <mangel.2711@gmail.com>
2020-09-30 15:47:59 -0400

    test callback pass

--- __test__/callbacks.test.js
+++ __test__/callbacks.test.js
@@ -3,7 +3,7 @@ import { callbackHell } from '../callbacks';
 describe('Probando un Callback', () => {
   test('Callback', (done) => {
     function otherCallback(data) {
-      expect(data).toBe('Hola Javascripters!');
+      expect(data).toBe('Hola Javascripters');
       done();
     }
     callbackHell(otherCallback);
